6558915cc73bed711a931b05b645d829c7db8270
yanyangtian1106 <1035282452@qq.com>
2022-10-24 13:58:14 +0800

    盒子模型定位练习1

---
+++ 盒子模型练习1.cpp
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "utf-8" / >
+<title>ͼƬ< / title>
+<style>
+*{
+	padding : 0;
+	margin : 0;
+}
+.box{
+	width:1000px;
+	height: 600px;
+	margin: 0 auto 0;
+	background - color: darkgrey;
+
+}
+.box1{
+	width:50 %;
+	height: 34 %;
+	background - color: darkcyan;
+	float: left;
+
+}
+.box2{
+	width:25 %;
+	height: 28 %;
+	background - color: chartreuse;
+	float: left;
+
+}
+.box3{
+	width:25 %;
+	height: 34 %;
+	background - color: salmon;
+	float: left;
+
+
+}
+.box4{
+	width:25 %;
+	height: 66 %;
+	background - color: seashell;
+	float: left;
+
+}
+.box5{
+	width:25 %;
+	height: 33 %;
+	background - color: fuchsia;
+	float: left;
+
+}
+.box6{
+	width:25 %;
+	height: 36 %;
+	background - color: aqua;
+	float: left;
+	margin - top: -4 %;
+
+}
+.box7{
+	width:25 %;
+	height: 33 %;
+	background - color: cornflowerblue;
+	float: left;
+
+}
+.box8{
+	width:25 %;
+	height: 33 %;
+	background - color: deepskyblue;
+	float: left;
+
+}
+.box9{
+	width:25 %;
+	height: 36 %;
+	background - color: salmon;
+	float: left;
+	margin - top: -2 %;
+
+}
+.box10{
+	width:25 %;
+	height: 33 %;
+	background - color: red;
+
+}
+
+< / style>
+< / head>
+<body>
+<div class = "box">
+< div class = "box1" >1 < / div >
+< div class = "box2">2 < / div >
+< div class = "box3">3 < / div >
+< div class = "box4">4 < / div >
+< div class = "box5">5 < / div >
+< div class = "box6 ">6 < / div >
+< div class = "box7">7 < / div >
+< div class = "box8">8 < / div >
+< div class = "box9">9 < / div >
+< div class = "box10">10 < / div >
+< / div>
+
+< / body>
+< / html>
