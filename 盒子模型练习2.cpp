2af2ca80405cbe2376fbbcbbf4354ce787247e9f
yanyangtian1106 <1035282452@qq.com>
2022-10-24 13:59:15 +0800

    盒子模型定位练习2

---
+++ 盒子模型练习2.cpp
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset = "utf-8" / >
+<title>< / title>
+<style>
+*{
+	padding : 0;
+	margin : 0;
+}
+.text{
+	width:1200px;
+	height:40px;
+	margin:20px auto 0;
+	/* background-color: coral; */
+}
+.box{
+	width:1200px;
+	height:300px;
+	margin: 20px auto 0;
+	position: relative;
+	/* background-color: blueviolet; */
+}
+.box1{
+	width:390px;
+	height:300px;
+	background - color: bisque;
+	float: left;
+	position: relative;
+}
+.box img{
+	width:100 %;
+	height:100 %;
+
+}
+.mr15{
+	margin - right: 15px;
+}
+.box2{
+	width:592.5px;
+	min - height: 300px;
+	background - color: #FF7F50;
+	float: left;
+	position: relative;
+}
+p{
+	margin - top: -8 %;
+/* 				position: absolute;
+				bottom: 5%; */
+				color: aliceblue;
+
+}
+h4{
+	position: absolute;
+	bottom: 15 %;
+	color: aliceblue;
+}
+< / style>
+< / head>
+<body>
+<!--文字所在的区域-->
+<div class = "text">
+<h2>精彩下一站，灵感这里找< / h2>
+< / div>
+
+<!--三张图片所在的区域-->
+<div class = "box">
+<div class = "box1 mr15">
+<img src = "img/1 (1).jpg" / >
+<h4> &nbsp; 情迷美利坚 | 美妙自然风景< / h4>
+<p> &nbsp; 如你没到美国，没为起美丽的大自然< / p>
+< / div>
+<div class = "box1 mr15">
+<img src = "img/3 (1).jpg" / >
+<h4> &nbsp; 情迷美利坚 | 美妙自然风景< / h4>
+<p> &nbsp; 如你没到美国，没为起美丽的大自然< / p>
+< / div>
+<div class = "box1">
+<img src = "img/2 (1).jpg" / >
+<h4>&nbsp; 情迷美利坚 | 美妙自然风景< / h4>
+<p>&nbsp; 如你没到美国，没为起美丽的大自然< / p>
+< / div>
+
+< / div>
+
+
+<!--两张图片所在的区域-->
+<div class = "box">
+<div class = "box2 mr15">
+<img src = "img/5.jpg" / >
+<h4>&nbsp; 情迷美利坚 | 美妙自然风景< / h4>
+<p> &nbsp; 如你没到美国，没为起美丽的大自然< / p>
+< / div>
+<div class = "box2">
+<img src = "img/7.jpg" / >
+<h4> &nbsp; 情迷美利坚 | 美妙自然风景< / h4>
+<p> &nbsp; 如你没到美国，没为起美丽的大自然< / p>
+< / div>
+< / div>
+< / body>
+< / html>
